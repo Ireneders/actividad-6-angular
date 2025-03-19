bd811c098abb2db4e85aedf7186f78c56ec35416
Ireneders <ireneders@gmail.com>
2025-03-19 19:51:43 +0100

    Creo rutas, creo y estilo el header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
+import { UserUpdateComponent } from './pages/user-update/user-update.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserFormComponent } from './pages/user-form/user-form.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
+import { UserUpdateComponent } from './pages/user-update/user-update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'user/:id', component: UserDetailComponent},	
+    {path: 'newuser', component: UserFormComponent},
+    {path: 'updateuser/:id', component: UserUpdateComponent},
+    {path: '**', redirectTo: 'home'}
+];
