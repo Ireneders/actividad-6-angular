5961596da35ffb1cedbed89e6adda19cd38ba3ca
Ireneders <ireneders@gmail.com>
2025-03-22 11:46:14 +0100

    elimino componentes innecesdarios y empiezo formularios

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -2,11 +2,11 @@ import { Component, inject, Input } from '@angular/core';
 import { IUser } from '../../interfaces/iuser.interface';
 import { UserService } from '../../services/user.service';
 import { IResponse } from '../../interfaces/iresponse.interface';
-import { UserFormComponentComponent } from '../../components/user-form-component/user-form-component.component';
+
 
 @Component({
   selector: 'app-user-form',
-  imports: [UserFormComponentComponent],
+  imports: [],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
